c573edb3ee77f861e82080613b89e27d012de916
admin <admin@DESKTOP-A940T65>
2023-11-28 10:07:59 +0900

    ㅇㅇ

--- hgjava/src/chap_06/Memeber_01.java
+++ hgjava/src/chap_06/Memeber_01.java
@@ -1,6 +1,14 @@
 package chap_06;
+//회원관리 앱
+//회원번호, 회원이름, 연락처, 성별
 
 public class Memeber_01 {
+	//속성(필드)
+	int memberNo;
+	String memberName;
+	String phone; // 휴대폰 번호 사이에 있는 - 하이픈이 문자로 인식되기때문에 string으로 설정
+	String membergender;
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
